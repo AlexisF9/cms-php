171106cf09f753f7f82c24a0fb909a9f0877d48f
Alexis <alexis.flacher38@gmail.com>
2021-12-02 11:28:38 +0100

    user

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -11,46 +11,4 @@ class Post
     private $author;
     private $titre;
 
-    public function getId() {
-        return $this->$id;
-    }
-    public function getImg()
-    {
-        return $this->$img;
-    }
-    public function getCreatedAt()
-    {
-        return $this->$createdAt;
-    }
-    public function getContent()
-    {
-        return $this->$content;
-    }
-    public function getAuthor()
-    {
-        return $this->$author;
-    }
-    public function getTitre()
-    {
-        return $this->$titre;
-    }
-
-
-    public function setImg($newImg)
-    {
-        $this->$img = $newImg;
-    }
-    public function setContent($newContent)
-    {
-        $this->$content = $newContent;
-    }
-    public function setAuthor($newAuthor)
-    {
-        $this->$author = $author;
-    }
-    public function setTitre($newTitre)
-    {
-        $this->$titre = $newTitre;
-    }
-    
 }
