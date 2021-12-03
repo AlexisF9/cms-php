73c6cdb85368a1c42242030b4d2d200cdd054d1e
Mathieu <mathieu.parinet@gmail.com>
2021-12-03 11:27:57 +0100

    create template & render

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -2,6 +2,23 @@
 
 class FrontController
 {
+
+    private $viewDir;
+    private $template;
+
+    public function __construct(){
+        $this->viewDir = "View/";
+        $this->template  = "View/template.view.php";
+    }
+
+    public function render(string $title, array $data, string $view){
+        $view = $this->viewDir . $view . ".view.php";
+        ob_start();
+        require $view;
+        $content = ob_get_clean();
+        return require $this->template;
+    }
+
     public function home(int $number = 5) {
         $manager = new PostManager(PDOFactory::getMySqlConnection());
         var_dump($manager->getAllPosts());
@@ -11,6 +28,10 @@ class FrontController
     {
         $managerPost = new PostManager(PDOFactory::getMySqlConnection());
         $managerComment = new CommentManager(PDOFactory::getMySqlConnection());
-        var_dump($managerPost->getPostbyId($id),$managerComment->getCommentByPostID($id));
+        $array = [$managerPost->getPostbyId($id),$managerComment->getCommentByPostID($id)];
+        // var_dump($array);
+
+        return $this->render("Article",$array,"Front/show");
+
     }
 }
--- src/index.php
+++ src/index.php
@@ -1,4 +1,3 @@
-<pre>
 <?php
 
 // faire un autoload avec composer
